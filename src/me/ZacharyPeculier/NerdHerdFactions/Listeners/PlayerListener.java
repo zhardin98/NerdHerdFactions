aeb38d3afdb3a419b06d7b0a7ad7ee8170496a5e
Zachary Hardin <zacharyjhardin@yahoo.com>
2021-06-18 19:06:08 -0500

    Implemented timer

--- src/me/ZacharyPeculier/NerdHerdFactions/Listeners/PlayerListener.java
+++ src/me/ZacharyPeculier/NerdHerdFactions/Listeners/PlayerListener.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.UUID;
 
 import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,81 +17,83 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PlayerListener implements Listener 
+public class PlayerListener implements Listener
 {
-	
 	private final Set<UUID> combatPlayers = new HashSet<UUID>();
 	private JavaPlugin plugin;
-	
-	public PlayerListener(JavaPlugin plugin) {
+	World world = plugin.getServer().getWorld("world");
+
+	public PlayerListener(JavaPlugin plugin)
+	{
 		this.plugin = plugin;
 	}
-	
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) 
-    {
-    	Player player = event.getPlayer();
-        
-        if (combatPlayers.contains(player.getUniqueId())) 
-        {
-        	player.getInventory().clear();
-        	player.setHealth(0);
-        	combatPlayers.remove(player.getUniqueId());
-        }
-    }
 
-    @EventHandler
-    public void onPlayerLeave(PlayerQuitEvent event) 
-    {
-    	Player player = event.getPlayer();
-        if (combatPlayers.contains(player.getUniqueId())) 
-        {
-            for (ItemStack itemStack : player.getInventory().getContents()) 
-            {
-            	if (itemStack == null) 
-            	{
-            		continue;
-            	}
-            	event.setQuitMessage(ChatColor.YELLOW + player.getName() + " combat logged");
-                player.getWorld().dropItemNaturally(player.getLocation(), itemStack);
-                player.getInventory().remove(itemStack);
-            }
-        }
-        else
-        {
-        	event.setQuitMessage(ChatColor.YELLOW + player.getName() + " has left the game");
-        }
-    }
-    
-    @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event) 
-    {
-    	Entity entity = event.getEntity();
-    	
-    	if (entity instanceof Player) 
-    	{
-    		Player player = (Player)entity;
-    		
-    		if (combatPlayers.contains(player.getUniqueId())) 
-    		{
-    			combatPlayers.remove(player.getUniqueId());
-    			event.setDeathMessage(player.getName() + " combat logged");
-    		}
-    	}
-    }
-    
-    private void addCombatPlayer(Player player) 
-    {
-    	if (combatPlayers.contains(player.getUniqueId())) 
-    	{
-    		return;
-    	}
-    	
-    	player.sendMessage(ChatColor.RED + "You are now in combat, if you leave you will be killed.");
-    	combatPlayers.add(player.getUniqueId());
-    }
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event)
+	{
+		Player player = event.getPlayer();
 
-    @EventHandler
+		if (combatPlayers.contains(player.getUniqueId()))
+		{
+			player.getInventory().clear();
+			player.setHealth(0);
+			combatPlayers.remove(player.getUniqueId());
+		}
+	}
+
+	@EventHandler
+	public void onPlayerLeave(PlayerQuitEvent event)
+	{
+		Player player = event.getPlayer();
+		UUID playerID = player.getUniqueId();
+		long tagTime = plugin.getConfig().getLong(playerID.toString() + ".combatStart");
+		if (combatPlayers.contains(playerID) && tagTime - world.getFullTime() <= 300)
+		{
+			for (ItemStack itemStack : player.getInventory().getContents())
+			{
+				if (itemStack == null)
+				{
+					continue;
+				}
+				event.setQuitMessage(ChatColor.YELLOW + player.getName() + " combat logged");
+				player.getWorld().dropItemNaturally(player.getLocation(), itemStack);
+				player.getInventory().remove(itemStack);
+			}
+		} else
+		{
+			event.setQuitMessage(ChatColor.YELLOW + player.getName() + " has left the game");
+		}
+	}
+
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent event)
+	{
+		Entity entity = event.getEntity();
+
+		if (entity instanceof Player)
+		{
+			Player player = (Player) entity;
+
+			if (combatPlayers.contains(player.getUniqueId()))
+			{
+				combatPlayers.remove(player.getUniqueId());
+				event.setDeathMessage(player.getName() + " combat logged");
+			}
+		}
+	}
+
+	private void addCombatPlayer(Player player)
+	{
+		if (combatPlayers.contains(player.getUniqueId()))
+		{
+			return;
+		}
+
+		player.sendMessage(ChatColor.RED + "You are now in combat, if you leave you will be killed.");
+		combatPlayers.add(player.getUniqueId());
+	}
+
+	@EventHandler
     public void onPvPDamage(EntityDamageByEntityEvent event)
     {
         Entity attacked = event.getEntity();
@@ -98,8 +101,13 @@ public class PlayerListener implements Listener
         
         if (attacked instanceof Player && attacker instanceof Player) 
         {
+        	UUID attackedID = attacked.getUniqueId();
+        	UUID attackerID = attacker.getUniqueId();
         	addCombatPlayer((Player)attacked);
         	addCombatPlayer((Player)attacker);
+        	plugin.getConfig().set(attackerID.toString() + ".combatStart", world.getFullTime());
+        	plugin.getConfig().set(attackedID.toString() + ".combatStart", world.getFullTime());
+        	plugin.saveConfig();
         }
     }
 }
